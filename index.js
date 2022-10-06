fedf2fd982878e8d5ef3f2841416fe11fc81753e
DispicableLee <lee.robert053@gmail.com>
2022-10-06 13:23:39 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+const currentUser = "Grace Hopper"
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+
+function shortGreeting(user){
+    const spliT = user.split("")
+    console.log(spliT)
+    return "Welcome, "+ spliT[0]
+}
+
+console.log(shortGreeting(currentUser))
